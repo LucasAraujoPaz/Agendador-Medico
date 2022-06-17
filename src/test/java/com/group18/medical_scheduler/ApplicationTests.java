37568f270a710016af059eba830dce5867295c14
Lucas Paz <lucasaraujopaz@hotmail.com>
2022-06-17 17:48:14 -0300

    Unit Test

--- src/test/java/com/group18/medical_scheduler/ApplicationTests.java
+++ src/test/java/com/group18/medical_scheduler/ApplicationTests.java
@@ -1,13 +1,24 @@
-package com.grupo18.agendador_medico;
+package com.group18.medical_scheduler;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class TesteApplicationTests {
-
+@AutoConfigureMockMvc
+class ApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/"))
+			.andExpect(status().isOk());
 	}
-
 }
